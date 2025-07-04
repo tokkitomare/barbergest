bf2bf4b3005e907ebf34aab7cbc2f7278cb1337e
Vitor <jvjv10112607@gmail.com>
2025-07-04 20:02:10 -0300

    feat: add layout

---
+++ frontend/src/pages/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/Nav";
+
+export default function Layout() {
+    return (
+        <>
+            <div className="flex h-screen flex-col">
+                <NavBar />
+                <div className="flex-1">
+                    <Outlet />
+                </div>
+            </div>
+        </>
+    );
+}
