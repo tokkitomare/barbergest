48f9e03134e96e19f291260192721e8e43a28a63
Vitor <jvjv10112607@gmail.com>
2025-07-04 19:58:49 -0300

    feat: add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+
 export default function App() {
   return (
       <>
-        <div className="flex justify-center items-center h-dvh">
-          <div className="bg-black/45 backdrop-blur-lg m-12 p-5 z-2 select-none shadow-2xl rounded-3xl hover:scale-105 transform transition-all duration-500 ease-in-out cursor-pointer">
-            <h1 className="font-huninn text-Helvetica text-4xl text-center">
-              EXEMPLO PARA VER SE O REACT + TAILWINDCSS TÁ FUNCIONANDO <span className="text-red-500">(PODE APAGAR E BOTAR O CÓDIGO NORMAL SE ESTIVER FUNCIONANDO)</span>
-            </h1>
-          </div>
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Layout /> }>
+              <Route index element={ <Home /> }/>
+              <Route path="*" element={ <NoPage /> }/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </>
   );
 }
